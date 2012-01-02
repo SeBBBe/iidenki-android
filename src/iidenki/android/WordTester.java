099afc86c2870ee75cc62e854443c44ecb8c2a01
Sebastian Fabian <sebbe@sebbe-T410i.(none)>
2012-01-02 02:47:50 +0100

    first commit

---
+++ src/iidenki/android/WordTester.java
@@ -0,0 +1,14 @@
+package iidenki.android;
+
+import iidenki.android.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WordTester extends Activity {
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	    setContentView(R.layout.vocabtest);
+	}
+}
---
+++ src/iidenki/android/iidenki.java
@@ -0,0 +1,40 @@
+package iidenki.android;
+
+import iidenki.android.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class iidenki extends Activity implements OnClickListener {
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	    setContentView(R.layout.mainmenu);
+	    
+	    Button vocabbutton = (Button) findViewById(R.id.button1);
+	    
+//	    vocabbutton.setOnClickListener(new OnClickListener() {
+//	        public void onClick(View view) {
+//	        	Intent intent = new Intent(Intent.ACTION_VIEW);
+//	        	intent.setClassName(this, WordTester.class.getName());
+//	        	startActivity(intent);
+//	          //Toast.makeText(getApplicationContext(), "Hello!", Toast.LENGTH_SHORT).show();
+//	        }
+//	      });
+    	
+    	vocabbutton.setOnClickListener(this);
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		Intent intent = new Intent(Intent.ACTION_VIEW);
+		intent.setClassName(this, WordTester.class.getName());
+		startActivity(intent);
+	}
+}
