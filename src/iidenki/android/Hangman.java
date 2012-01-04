3871c25ca36cad37715b19f26808fa5a9b463462
Sebastian Fabian <sjlfabian@gmail.com>
2012-01-05 00:27:05 +0100

    hangman fully working

--- src/iidenki/android/Hangman.java
+++ src/iidenki/android/Hangman.java
@@ -4,35 +4,31 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
-
 import control.GameHandler;
-
 import vocab.Word;
 import iidenki.android.R;
 import android.app.Activity;
-import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Hangman extends Activity implements OnClickListener{
 	
-	GameHandler gh;
+	private GameHandler gh;
+	private int level;
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 	    setContentView(R.layout.hangman);
-	    int level = 1;
+	    level = 1;
 	    
 	    String fil = getIntent().getStringExtra("file");
 	    File readfrom = new File(fil);
@@ -69,13 +65,58 @@ public class Hangman extends Activity implements OnClickListener{
 	 * Make a guess
 	 */
 	public void onClick(View arg0) {
+		InputMethodManager imm = (InputMethodManager) getSystemService(
+			    INPUT_METHOD_SERVICE);
+		imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+
 		EditText textbox = (EditText) findViewById(R.id.editText1);
 		String answer = textbox.getText().toString();
 		textbox.setText("");
-		gh.guess(answer);
+		int lols = gh.guess(answer);
+		if (lols == 1){
+			Toast.makeText(getApplicationContext(), "Correct!", Toast.LENGTH_SHORT).show();
+		}else if(lols == 0){
+			Toast.makeText(getApplicationContext(), "Sorry!", Toast.LENGTH_SHORT).show();
+			level++;
+			updatePic();
+		}
+		else if(lols == 3){
+			Toast.makeText(getApplicationContext(), "Input exactly one character please!", Toast.LENGTH_SHORT).show();
+		}
 		TextView box = (TextView) findViewById(R.id.textView1);
 	    box.setText(gh.partialText());
 	    box = (TextView) findViewById(R.id.textView3);
 	    box.setText(gh.wrongText());
+	    
+	    String p = gh.partialText();
+		if (!p.contains("_")){
+			Toast.makeText(getApplicationContext(), "Congratulations!", Toast.LENGTH_SHORT).show();
+			disableInput();
+		}
+	}
+
+	private void updatePic() {
+		ImageView img = (ImageView) findViewById(R.id.imageView1);
+		switch(level){
+			case 1: img.setImageResource(R.drawable.hangman1); break;
+			case 2: img.setImageResource(R.drawable.hangman2); break;
+			case 3: img.setImageResource(R.drawable.hangman3); break;
+			case 4: img.setImageResource(R.drawable.hangman4); break;
+			case 5: img.setImageResource(R.drawable.hangman5); break;
+			case 6: img.setImageResource(R.drawable.hangman6); break;
+			case 7: img.setImageResource(R.drawable.hangman7); break;
+			case 8: img.setImageResource(R.drawable.hangman8); break;
+			case 9: img.setImageResource(R.drawable.hangman9); break;
+			case 10: img.setImageResource(R.drawable.hangman10); 
+						disableInput();
+						break;
+		}
+	}
+
+	private void disableInput() {
+		Button b = (Button) findViewById(R.id.button1);
+		b.setVisibility(View.INVISIBLE);
+		EditText textbox = (EditText) findViewById(R.id.editText1);
+		textbox.setVisibility(View.INVISIBLE);
 	}
 }
