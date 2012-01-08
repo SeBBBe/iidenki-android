ada095f07a99bff6fd4c7492d64a701316261eb0
Sebastian Fabian <sjlfabian@gmail.com>
2012-01-08 23:28:16 +0100

    falling kanji finally works properly

--- gen/iidenki/android/R.java
+++ gen/iidenki/android/R.java
@@ -29,9 +29,6 @@ public final class R {
         public static final int iidenki_plain=0x7f02000b;
     }
     public static final class id {
-        public static final int TextView01=0x7f060009;
-        public static final int TextView02=0x7f060008;
-        public static final int TextView03=0x7f060007;
         public static final int button1=0x7f060006;
         public static final int button2=0x7f06000c;
         public static final int button3=0x7f06000d;
@@ -42,6 +39,9 @@ public final class R {
         public static final int linearLayout1=0x7f060004;
         public static final int linearLayout2=0x7f06000b;
         public static final int rain01=0x7f06000a;
+        public static final int rain02=0x7f060009;
+        public static final int rain03=0x7f060008;
+        public static final int rain04=0x7f060007;
         public static final int spinner1=0x7f06000e;
         public static final int textView1=0x7f060002;
         public static final int textView2=0x7f060001;
--- src/iidenki/android/KanjiTester.java
+++ src/iidenki/android/KanjiTester.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import iidenki.android.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
@@ -26,52 +27,46 @@ public class KanjiTester extends Activity implements OnClickListener{
 	    setClickListener();
 	    
 	    startAnimation(R.id.rain01);
-//	    startAnimation(R.id.rain02);
-//	    startAnimation(R.id.rain03);
-//	    startAnimation(R.id.rain04);
-//	    startAnimation(R.id.rain05);
-//	    startAnimation(R.id.rain06);
-//	    startAnimation(R.id.rain07);
-//	    startAnimation(R.id.rain08);
+	    startAnimation(R.id.rain02);
+	    startAnimation(R.id.rain03);
+	    startAnimation(R.id.rain04);
 	}
 
 	private void startAnimation(int v) {
 		TextView test = (TextView) findViewById(v);
-		LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(0, 2000);
 		
 		Random rand = new Random();
-	    ta = new TranslateAnimation(0, 0, 0, 200);
+	    ta = new TranslateAnimation(0, 0, 0, 800);
 	    ta.setDuration(6000 + rand.nextInt(5000));
 	    ta.setFillAfter(true);
 
 	    test.startAnimation(ta);
-	    //test.setLayoutParams(lp);
 	}
 
 	private void setClickListener() {
+		Display display = getWindowManager().getDefaultDisplay(); 
+		int width = display.getWidth();
+		int x = width/4;
+		int y =  display.getHeight() * 2;
+		
 		TextView test = (TextView) findViewById(R.id.rain01);
-		test.setLayoutParams(new LinearLayout.LayoutParams(100,100));
+		test.setLayoutParams(new LinearLayout.LayoutParams(x,y));
 	    test.setOnClickListener(this);
-//	    test = (TextView) findViewById(R.id.rain02);
-//	    test.setOnClickListener(this);
-//	    test = (TextView) findViewById(R.id.rain03);
-//	    test.setOnClickListener(this);
-//	    test = (TextView) findViewById(R.id.rain04);
-//	    test.setOnClickListener(this);
-//	    test = (TextView) findViewById(R.id.rain05);
-//	    test.setOnClickListener(this);
-//	    test = (TextView) findViewById(R.id.rain06);
-//	    test.setOnClickListener(this);
-//	    test = (TextView) findViewById(R.id.rain07);
-//	    test.setOnClickListener(this);
-//	    test = (TextView) findViewById(R.id.rain08);
-//	    test.setOnClickListener(this);
+	    test = (TextView) findViewById(R.id.rain02);
+	    test.setLayoutParams(new LinearLayout.LayoutParams(x,y));
+	    test.setOnClickListener(this);
+	    test = (TextView) findViewById(R.id.rain03);
+	    test.setLayoutParams(new LinearLayout.LayoutParams(x,y));
+	    test.setOnClickListener(this);
+	    test = (TextView) findViewById(R.id.rain04);
+	    test.setLayoutParams(new LinearLayout.LayoutParams(x,y));
+	    test.setOnClickListener(this);
 	}
 	
 	/**
 	 * Item clicked
 	 */
 	public void onClick(View arg0) {
-		Toast.makeText(getApplicationContext(), "click!", Toast.LENGTH_SHORT).show();
+		Toast.makeText(getApplicationContext(), ((TextView)arg0).getText(), Toast.LENGTH_SHORT).show();
 	}
 }
