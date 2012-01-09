ac28d984d25a10767644414a6b30d8f64d26a24f
Sebastian Fabian <sjlfabian@gmail.com>
2012-01-09 17:04:21 +0100

    lock orientation for main menu

--- src/iidenki/android/iidenki.java
+++ src/iidenki/android/iidenki.java
@@ -4,6 +4,7 @@ import iidenki.android.R;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -14,6 +15,7 @@ public class iidenki extends Activity implements OnClickListener {
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 	    setContentView(R.layout.mainmenu);
 	    
 	    Button b = (Button) findViewById(R.id.button1);
